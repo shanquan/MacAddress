ad8d62b52a612f8cca7a7a4bc46309d26bdaa2c7
shanquane <2495742347@qq.com>
2018-07-20 15:25:34 +0800

    feat:add eth0 address

--- src/android/MacAddressPlugin.java
+++ src/android/MacAddressPlugin.java
@@ -84,20 +84,20 @@ public class MacAddressPlugin extends CordovaPlugin {
      * 
      * @return the mac address
      */
-    private String getLegacyMacAddress() {
+    // private String getLegacyMacAddress() {
 
-        String macAddress = null;
+    //     String macAddress = null;
 
-        WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        macAddress = wm.getConnectionInfo().getMacAddress();
+    //     WifiManager wm = (WifiManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    //     macAddress = wm.getConnectionInfo().getMacAddress();
 
-        if (macAddress == null || macAddress.length() == 0) {
-            macAddress = "02:00:00:00:00:00";
-        }
+    //     if (macAddress == null || macAddress.length() == 0) {
+    //         macAddress = "02:00:00:00:00:00";
+    //     }
 
-        return macAddress;
+    //     return macAddress;
 
-    }
+    // }
 
     /**
      * Gets the mac address on version >= Marshmallow.
@@ -132,4 +132,51 @@ public class MacAddressPlugin extends CordovaPlugin {
 
         return "02:00:00:00:00:00";
     }
+    private String getLegacyMacAddress(){
+        String str="";
+        String macSerial="";
+        try {
+            Process pp = Runtime.getRuntime().exec(
+                    "cat /sys/class/net/wlan0/address ");
+            InputStreamReader ir = new InputStreamReader(pp.getInputStream());
+            LineNumberReader input = new LineNumberReader(ir);
+
+            for (; null != str;) {
+                str = input.readLine();
+                if (str != null) {
+                    macSerial = str.trim();// 去空格
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (macSerial == null || "".equals(macSerial)) {
+            try {
+                return loadFileAsString("/sys/class/net/eth0/address")
+                        .toUpperCase().substring(0, 17);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+        }
+        return macSerial;
+    }
+    private String loadFileAsString(String fileName) throws Exception {
+            FileReader reader = new FileReader(fileName);
+            String text = loadReaderAsString(reader);
+            reader.close();
+            return text;
+        }
+    private String loadReaderAsString(Reader reader) throws Exception {
+            StringBuilder builder = new StringBuilder();
+            char[] buffer = new char[4096];
+            int readLength = reader.read(buffer);
+            while (readLength >= 0) {
+                builder.append(buffer, 0, readLength);
+                readLength = reader.read(buffer);
+            }
+            return builder.toString();
+        }
 }
